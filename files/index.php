888e4b39fc7f9c2e3a3a307aa98cfd0048e169a0
Jerome Tabiri <jtabiri@gmail.com>
2021-11-21 11:20:07 -0500

    workstation playbook works

--- files/index.php
+++ files/index.php
@@ -1,1 +1,10 @@
-<?php phpinfo(); ?>
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'admin';
+$dbpass = 'password';
+$dbname = 'test';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Error connecting to mysql');
+
+?>
